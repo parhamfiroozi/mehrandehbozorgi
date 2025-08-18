c58953ce3376d09627162d7242b9e48465e9921b
parhamfiroozi <apple@Apples-MacBook-Pro.local>
2025-08-19 00:15:36 +0330

    Initial commit (clean, no node_modules)

--- src/app/(lang)/[locale]/layout.tsx
+++ src/app/(lang)/[locale]/layout.tsx
@@ -1,17 +1,17 @@
+// src/app/(lang)/[locale]/layout.tsx
 import type { ReactNode } from "react";
 
-export default function LocaleLayout({
-  children,
-  params: { locale },
-}: {
-  children: ReactNode;
-  params: { locale: "fa" | "en" | "fr" };
-}) {
-  const isFa = locale === "fa";
+export default function LocaleLayout(props: any) {
+  const { children, params } = props as {
+    children: ReactNode;
+    params: { locale: "fa" | "en" | "fr" };
+  };
+  const isFa = params.locale === "fa";
+
   return (
     <div
       dir={isFa ? "rtl" : "ltr"}
-      data-locale={locale}
+      data-locale={params.locale}
       className={isFa ? "font-fa" : "font-sans"}
     >
       {children}
