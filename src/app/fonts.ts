9ee92540aca2c4c7d14efa87a0399fc235b028c9
parhamfiroozi <apple@Apples-MacBook-Pro.local>
2025-08-19 09:08:08 +0330

    Initial commit (clean, no node_modules)

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,15 +1,23 @@
+// src/app/fonts.ts
 import localFont from "next/font/local";
 
-// Default Persian UI font
+// BNazanin
 export const fontFa = localFont({
-  src: [{ path: "./fonts/BNazanin.ttf", weight: "400", style: "normal" }],
+  src: [{ path: "./BNazanin.ttf", weight: "400", style: "normal" }],
   variable: "--font-fa",
   display: "swap",
 });
 
-// Headings/motto font
+// Cordoba
 export const fontCordoba = localFont({
-  src: [{ path: "./fonts/Mj_Cordoba.ttf", weight: "400", style: "normal" }],
+  src: [{ path: "./Mj_Cordoba.ttf", weight: "400", style: "normal" }],
   variable: "--font-cordoba",
+  display: "swap",
+});
+
+// Optional: Hajar
+export const fontHajar = localFont({
+  src: [{ path: "./Mj_Hajar_0.ttf", weight: "400", style: "normal" }],
+  variable: "--font-hajar",
   display: "swap",
 });
