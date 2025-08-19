26f31d6ac578a36bf2e87e0dd7b00becf7f54dc1
parhamfiroozi <apple@Apples-MacBook-Pro.local>
2025-08-19 09:30:24 +0330

    Add Persian font files and update fonts.ts

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,23 +1,13 @@
-// src/app/fonts.ts
 import localFont from "next/font/local";
 
-// BNazanin
 export const fontFa = localFont({
-  src: [{ path: "./BNazanin.ttf", weight: "400", style: "normal" }],
+  src: [{ path: "./fonts/BNazanin.ttf", weight: "400", style: "normal" }],
   variable: "--font-fa",
   display: "swap",
 });
 
-// Cordoba
 export const fontCordoba = localFont({
-  src: [{ path: "./Mj_Cordoba.ttf", weight: "400", style: "normal" }],
+  src: [{ path: "./fonts/Mj_Cordoba.ttf", weight: "400", style: "normal" }],
   variable: "--font-cordoba",
-  display: "swap",
-});
-
-// Optional: Hajar
-export const fontHajar = localFont({
-  src: [{ path: "./Mj_Hajar_0.ttf", weight: "400", style: "normal" }],
-  variable: "--font-hajar",
   display: "swap",
 });
